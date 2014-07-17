fa0996fa5b620836695b4247bafda23bb6f3c70c
kristiyankisimov <kristiyankisimov@yahoo.com>
2014-07-17 21:00:33 +0300

    Change protocol files

--- RBI-Protocol/src/com/hackbulgaria/corejava/RBIProtocol.java
+++ RBI-Protocol/src/com/hackbulgaria/corejava/RBIProtocol.java
@@ -14,21 +14,27 @@ public class RBIProtocol {
         final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         final StringBuilder builder = new StringBuilder();
 
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            builder.append(line + System.lineSeparator());
-            line = null;
-        }
-        builder.append(RBIConstants.EOM);
-        reader.close();
+        String line;
+        do {
+            line = reader.readLine();
+            if (line.equals(RBIConstants.EOM)) {
+                builder.append(line + System.lineSeparator());
+            } else {
+                builder.append(line);
+            }
+
+        } while (!line.equals(RBIConstants.EOM));
+        // builder.append(RBIConstants.EOM);
+
         return builder.toString();
     }
 
     public static void writeRBIMessage(String message, Socket connection) throws IOException {
         final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        message = message + System.lineSeparator() + RBIConstants.EOM;
         writer.write(message);
         writer.flush();
-        writer.close();
+        // writer.close();
     }
 
 }
